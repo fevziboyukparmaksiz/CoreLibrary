7c0d52c7b9583ff4d4062328bcf8c708a3ff239d
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-08-10 13:26:48 +0300

    Automapper - mapping different property names

--- FluentValidationApp.Web/DTOs/CustomerDto.cs
+++ FluentValidationApp.Web/DTOs/CustomerDto.cs
@@ -5,8 +5,8 @@ namespace FluentValidationApp.Web.DTOs
     public class CustomerDto
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public int? Age { get; set; }
+        public string? Isim { get; set; }
+        public string? Eposta { get; set; }
+        public int? Yas { get; set; }
     }
 }
--- FluentValidationApp.Web/Mapping/CustomerProfile.cs
+++ FluentValidationApp.Web/Mapping/CustomerProfile.cs
@@ -8,7 +8,11 @@ namespace FluentValidationApp.Web.Mapping
     {
         public CustomerProfile()
         {
-            CreateMap<Customer, CustomerDto>().ReverseMap();
+            CreateMap<Customer, CustomerDto>()
+                .ForMember(dest => dest.Isim, opt => opt.MapFrom(x => x.Name))
+                .ForMember(dest => dest.Eposta, opt => opt.MapFrom(x => x.Email))
+                .ForMember(dest => dest.Yas, opt => opt.MapFrom(x => x.Age));
+                
         }
     }
 }
