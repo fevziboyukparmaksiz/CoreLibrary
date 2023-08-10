8eb9813adac13272bd072c5202aa1e46c2baf81d
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-08-10 14:36:28 +0300

    AutoMapper - Include Member

--- FluentValidationApp.Web/DTOs/CustomerDto.cs
+++ FluentValidationApp.Web/DTOs/CustomerDto.cs
@@ -9,7 +9,7 @@ namespace FluentValidationApp.Web.DTOs
         public string? Eposta { get; set; }
         public int? Yas { get; set; }
         public string FullName { get; set; }
-        public string CCNumber { get; set; }
-        public DateTime CCValidDate { get; set; }
+        public string Number { get; set; }
+        public DateTime ValidDate { get; set; }
     }
 }
